066bdc1a60a6507eecd79ce1209ea4085771fe61
deni-cyber <deniskamau928@gmial.com>
2024-11-28 12:00:32 +0300

    improved the UI

--- artstore/settings.py
+++ artstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-woi7_z4h%kavpmuuk^17h9@0i&j$x)4rp^wn(@pjm=oufz*g#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','192.168.0.102']
+ALLOWED_HOSTS = ['localhost','192.168.0.102','127.0.0.1' ]
 
 
 # Application definition
